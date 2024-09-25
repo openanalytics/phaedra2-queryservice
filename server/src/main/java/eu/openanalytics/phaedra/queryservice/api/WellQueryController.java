d4c17d14996d1c354d3bbba28accc6e6ab64c402
sberberovic <sasa.berberovic@openanalytics.eu>
2024-09-25 13:31:11 +0200

    update: add filters by well type, substance name and metadata

--- server/src/main/java/eu/openanalytics/phaedra/queryservice/api/WellQueryController.java
+++ server/src/main/java/eu/openanalytics/phaedra/queryservice/api/WellQueryController.java
@@ -1,9 +1,9 @@
 package eu.openanalytics.phaedra.queryservice.api;
 
-import eu.openanalytics.phaedra.plateservice.client.PlateServiceClient;
 import eu.openanalytics.phaedra.plateservice.client.PlateServiceGraphQLClient;
 import eu.openanalytics.phaedra.plateservice.dto.WellDTO;
 import eu.openanalytics.phaedra.queryservice.record.WellFilter;
+import eu.openanalytics.phaedra.queryservice.utils.FilterUtils;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.graphql.data.method.annotation.Argument;
@@ -47,6 +47,26 @@ public class WellQueryController {
         } else if (wellFilter.id().in() != null) {
           results.addAll(plateServiceGraphQLClient.getWellsByExperimentIds(wellFilter.experimentId().in()));
         }
+      }
+
+      if (wellFilter.wellType() != null) {
+        results = FilterUtils.filterByString(results, wellFilter.wellType(), WellDTO::getWellType);
+      }
+      if (wellFilter.wellSubstance() != null) {
+        results = FilterUtils.filterByString(results, wellFilter.wellSubstance(), wellDTO -> wellDTO.getWellSubstance().getName());
+      }
+      if (wellFilter.status() != null) {
+        results = results.stream()
+            .filter(wellDTO -> wellDTO.getStatus().equals(wellFilter.status()))
+            .toList();
+      }
+
+      if (wellFilter.tags() != null) {
+        results = FilterUtils.filterByMetaData(results, wellFilter.tags(), WellDTO::getTags);
+      }
+      if (wellFilter.properties() != null) {
+        results = FilterUtils.filterByMetaData(results, wellFilter.properties(),
+            e -> e.getProperties().stream().map(prop -> prop.propertyName()).toList());
       }
     }
 
